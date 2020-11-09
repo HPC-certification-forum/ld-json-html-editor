5473eaaf9680a35de9a893d743742feb4590b75d
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-11-09 19:10:46 +0000

    Primitive renderer.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a Javascript editor for LD-JSON.
 It aims to create annotations compatible with schema.org.
 During the deployment, a schema can be set that defines which fields to show and which (selectable) options to give.
+
+To embed it into your webpage, e.g., use: <script type="application/ld+json">
--- ld-json.js
+++ ld-json.js
@@ -1,6 +1,29 @@
 var ldJSON = {
+  appendType : function(type, txt){
+    return "<a href='https://schema.org/" + type + "'>" + txt + "</a>";
+  } ,
+
   appendSection : function(data){
-    return "<h2>Test</h2>";
+    var str = "";
+    for(key in data){
+      var val = data[key];
+      if (val instanceof Object){
+        var link = key;
+        var type = "@type" in val ? val["@type"] : "";
+        if(type != ""){
+          link = ldJSON.appendType(type, key + " : " + type);
+          if("additionalType" in val){
+            link += ", " + ldJSON.appendType(val["additionalType"], val["additionalType"]);
+          }
+        }
+        str += "<div class='ldsection'><div class='ldhead'>" + link + "</div><div class='ldbody'>" + ldJSON.appendSection(val) + "</div></div>";
+      }else if(key[0] != "@" && key != "additionalType"){
+        str += key + " = ";
+        str += val + "<br/>";
+      }
+    }
+
+    return str;
   },
 
   showEditor : function(divName, schemaURL){
